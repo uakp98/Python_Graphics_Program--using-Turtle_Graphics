821fa711da01170479d4a867fa7239662ad324f9
Apurba Kumar Pramanik <59066595+uakp98@users.noreply.github.com>
2022-07-05 18:53:13 +0530

    Add files via upload

---
+++ Hexagon Draw_TurtleGraphics.py
@@ -0,0 +1,8 @@
+#How to Draw a Hexagon using Python Turtle !
+import turtle
+ninja=turtle.Turtle()
+#ninja.speed(10)
+
+for i in range(6):
+    ninja.forward(50)
+    ninja.right(300)
---
+++ Triangle Draw_TurtleGraphics.py
@@ -0,0 +1,11 @@
+#How to Draw a Hexagon using Python Turtle !
+import turtle
+ninja=turtle.Turtle()
+#ninja.speed(10)
+
+for i in range(3):
+    ninja.forward(120)
+    ninja.right(240)
+
+turtle.done()
+#turtle.exitonclick()
