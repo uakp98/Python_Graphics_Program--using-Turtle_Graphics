426b080e98d7c7c27364f9f74eaadd82766373c3
Apurba Kumar Pramanik <59066595+uakp98@users.noreply.github.com>
2022-07-07 02:22:11 +0530

    Add files via upload

---
+++ Square_or_quadrilateral_TurtleGraphics.py
@@ -0,0 +1,9 @@
+import turtle
+ninja=turtle.Turtle()
+ninja.speed(10)
+
+for i in range(4):
+    ninja.forward(120)
+    ninja.right(270)
+    
+turtle.done()
---
+++ pentagon_TurtleGraphics.py
@@ -0,0 +1,9 @@
+import turtle
+ninja=turtle.Turtle()
+ninja.speed(10)
+
+for i in range(5):
+    ninja.forward(100)
+    ninja.right(288)
+
+turtle.done()
