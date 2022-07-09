6410cce34a1043e88447914421f3e441717c2bd9
Apurba Kumar Pramanik <59066595+uakp98@users.noreply.github.com>
2022-07-09 16:29:49 +0530

    Add files via upload

---
+++ heptagon_TurtleGraphics.py
@@ -0,0 +1,9 @@
+import turtle
+ninja=turtle.Turtle()
+ninja.speed(10)
+
+for i in range(7):
+    ninja.forward(100)
+    ninja.right(308.5)
+   
+turtle.done()
---
+++ octagon_TurtleGraphics.py
@@ -0,0 +1,9 @@
+import turtle
+ninja=turtle.Turtle()
+ninja.speed(10)
+
+for i in range(8):
+    ninja.forward(100)
+    ninja.right(315)
+
+turtle.done()
